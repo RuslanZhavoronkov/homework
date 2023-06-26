75d54f62cca280e28c5b21976ea70486551854e1
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-26 15:41:00 +0300

    HW1

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,40 +1,56 @@
-import React from 'react'
-import s from './Message.module.css'
+import React from "react";
+import { MessageType } from "../HW1";
+import s from "./Message.module.css";
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = {
+  message: MessageType;
+};
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
-    return (
-        <div id={'hw1-message-' + props.message.id} className={s.message}>
-            <div className={s.imageAndText}>
-                <img
-                    id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
-                />
-                <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </pre>
-                </div>
-            </div>
-            <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
+  return (
+    <div
+      id={props.message.message.text + props.message.id}
+      className={s.message}
+    >
+      <div className={s.imageAndText}>
+        <img
+          id={props.message.user.avatar + props.message.id}
+          // создаёт студент
+          src={props.message.user.avatar}
+          alt={props.message.user.name}
+          //
+        />
+        <div className={s.text}>
+          <div
+            id={props.message.user.name + props.message.id}
+            className={s.name}
+          >
+            {/*создаёт студент*/}
+            {props.message.user.name}
+            {/**/}
+          </div>
+          <pre
+            id={props.message.message.text + props.message.id}
+            className={s.messageText}
+          >
+            {/*создаёт студент*/}
+            {props.message.message.text}
+            {/**/}
+          </pre>
         </div>
-    )
-}
+      </div>
+      <div
+        id={props.message.message.time + props.message.id}
+        className={s.time}
+      >
+        {/*создаёт студент*/}
+        {props.message.message.time}
+        {/**/}
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
